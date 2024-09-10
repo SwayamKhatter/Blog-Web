251f18cfcc1d234014031f3ef98386772c9cccad
Swayam Khatter <khatterswayam@gmail.com>
2024-09-10 15:54:29 +0530

    bug fix

--- Blog/src/components/ArticleList.jsx
+++ Blog/src/components/ArticleList.jsx
@@ -63,10 +63,10 @@ const ArticleList = () => {
 
   return (
     <div className="container mx-auto py-10">
-      <h1 className="text-3xl font-semibold mb-8">Articles</h1>
+      <h1 className="text-3xl font-semibold mb-8">Add a New Article</h1>
 
       <div className="mb-8">
-        <h2 className="text-2xl font-semibold">Add a New Article</h2>
+        <h2 className="text-2xl font-semibold"></h2>
         {errorMessage && <p className="text-red-500">{errorMessage}</p>}
         <form onSubmit={handleSubmit} className="mb-6">
           <div className="mb-4">
